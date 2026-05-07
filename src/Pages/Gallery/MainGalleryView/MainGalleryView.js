4814c8edee6ecd6d6ffc357129c4a1c8bc4938c2
khuynh16 <kelvinahuynh@gmail.com>
2026-05-07 15:42:20 -0400

    hooking up back to gallery button

--- src/Pages/Gallery/MainGalleryView/MainGalleryView.js
+++ src/Pages/Gallery/MainGalleryView/MainGalleryView.js
@@ -1,13 +1,29 @@
 import ArtCard from "../../../Components/Card/Card";
 import styles from "./MainGalleryView.module.css";
+import arrowLeft from "../../../Assets/Images/arrow-left.png";
+import { useNavigate } from "react-router-dom";
 
 const MainGalleryView = ({ initialArtworkSlug }) => {
+  const navigate = useNavigate();
   return (
     <>
       <div className={styles.outerContainer}>
         <div className={styles.innerContainer}>
           <ArtCard initialArtworkSlug={initialArtworkSlug} />
         </div>
+        {initialArtworkSlug && (
+          <button
+            className={styles.backToGalleryButton}
+            onClick={() => navigate("/gallery")}
+          >
+            <img
+              className={styles.backArrow}
+              src={arrowLeft}
+              alt="Back arrow https://www.flaticon.com/free-icons/arrow-left created by deha21"
+            ></img>
+            Back to Main Gallery
+          </button>
+        )}
       </div>
     </>
   );
